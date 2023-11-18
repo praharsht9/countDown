2e1adbe49883d5eccafcd23675aa946680f6faab
praharsht9 <praharsht9@gmail.com>
2023-11-19 00:00:49 +0530

    Countdown code

---
+++ src/CountDown.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './countdown.css';
+
+const CountDown = ({ seconds }) => {
+	const [countDown, setCountDown] = useState(seconds);
+	const timerId = useRef();
+
+	const formatTime = (time) => {
+		let minutes = Math.floor(time / 60);
+		let seconds = Math.floor(time - minutes * 60);
+		if (minutes <= 10) minutes = '0' + minutes;
+		if (seconds <= 10) seconds = '0' + seconds;
+
+		let fontColor = 'white'; // Default font color
+		if (time <= 5) {
+			fontColor = 'red'; // Change font color to red
+		}
+
+		return (
+			<span style={{ color: fontColor }}>
+				{minutes}:{seconds}
+			</span>
+		);
+	};
+
+	useEffect(() => {
+		timerId.current = setInterval(() => {
+			setCountDown((prev) => prev - 1);
+		}, 1000);
+		return () => clearInterval(timerId.current);
+	}, []);
+
+	useEffect(() => {
+		if (countDown <= 0) {
+			clearInterval(timerId.current);
+			alert('END');
+		}
+	});
+
+	return (
+		<div>
+			<h1 className='timer'>{formatTime(countDown)}</h1>
+		</div>
+	);
+};
+
+export default CountDown;
